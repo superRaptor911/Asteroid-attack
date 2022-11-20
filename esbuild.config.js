cfac030a5c2dd16e860df65f040d6fcf31e3ee97
superRaptor911 <raptor.inc2018@gmail.com>
2022-11-20 21:16:17 +0530

    Esbuild copy plugin

---
+++ esbuild.config.js
@@ -0,0 +1,19 @@
+import { build } from 'esbuild';
+import esbuildMxnCopy from 'esbuild-plugin-mxn-copy';
+
+(async () => {
+  const res = await build({
+    entryPoints: ['./src/main.ts'],
+    bundle: true,
+    outfile: './build/src/main.js',
+    plugins: [
+      esbuildMxnCopy({
+        copy: [
+          // You can include files & directories
+          { from: 'src/assets', to: 'build/assets' },
+        ],
+        verbose: true,
+      }),
+    ],
+  });
+})();
