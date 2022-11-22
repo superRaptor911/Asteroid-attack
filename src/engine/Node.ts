b58fe9674bee210c6ce80a954c6578504624e64c
superRaptor911 <raptor.inc2018@gmail.com>
2022-11-22 11:52:56 +0530

    Added name property

--- src/engine/Node.ts
+++ src/engine/Node.ts
@@ -3,21 +3,58 @@ import * as THREE from 'three';
 export class SceneNode {
   scene: THREE.Scene;
   parent: SceneNode | null = null;
-  children: SceneNode[] = [];
+  _children: SceneNode[] = [];
+  _childMap: { [key: string]: SceneNode } = {};
+  _name = '';
 
-  constructor() {
+  constructor(name = '') {
     this.scene = new THREE.Scene();
+    this._name = name;
+  }
+
+  get name(): string {
+    return this._name;
+  }
+
+  set name(name: string) {
+    if (this.parent) {
+      if (this.parent._childMap[this._name] === this) {
+        delete this.parent._childMap[this._name];
+      }
+
+      if (this.parent._childMap[name]) {
+        this._name = `${name}_${this._children.length}`;
+      }
+      return;
+    }
+    this._name = name;
   }
 
   addChild(child: SceneNode): void {
     if (child.parent !== null) {
       child.parent.removeChild(child);
     }
+
+    // Check for name collisions
+    if (this._childMap[child._name]) {
+      child._name = `${child._name}_${this._children.length}`;
+    }
+
     child.parent = this;
-    this.children.push(child);
+    this._childMap[child._name] = child; // Add to child map
+    this._children.push(child); // Add to children array
 
     this.scene.add(child.scene);
     child.onReady();
+  }
+
+  getChild(name: string): SceneNode | null {
+    const indexOf = name.indexOf('/');
+    if (indexOf === -1) {
+      return this._childMap[name] || null;
+    }
+    const childName = name.substring(indexOf);
+    return this._childMap[childName].getChild(name.substring(indexOf + 1));
   }
 
   onReady(): void {
@@ -32,17 +69,18 @@ export class SceneNode {
     if (child.parent === this) {
       this.scene.remove(child.scene);
       child.parent = null;
+      delete this._childMap[child._name];
+      this._children.splice(this._children.indexOf(child), 1);
       child.onRemove();
-      this.children.splice(this.children.indexOf(child), 1);
     }
   }
 
   update(delta: number): void {
-    this.children.forEach((child) => child.update(delta));
+    this._children.forEach((child) => child.update(delta));
   }
 
   destroy(): void {
-    this.children.forEach((child) => child.destroy());
-    this.children = [];
+    this._children.forEach((child) => child.destroy());
+    this._children = [];
   }
 }
