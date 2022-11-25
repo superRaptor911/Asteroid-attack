3e60b20ccf931407ed2cfa87f1a7681dc1965307
superRaptor911 <raptor.inc2018@gmail.com>
2022-11-25 15:58:58 +0530

    float2Int

--- src/engine/utils.ts
+++ src/engine/utils.ts
@@ -1,3 +1,7 @@
 export const randRange = (a: number, b: number): number => {
   return a + Math.random() * (b - a);
 };
+
+export const float2Int = (a: number): number => {
+  return a < 0 ? Math.ceil(a) : a | 0;
+};
