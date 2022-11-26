7d6e284ef6725c69241b56e5f237e9c3204be0e7
superRaptor911 <raptor.inc2018@gmail.com>
2022-11-26 21:07:50 +0530

    Removed auto cleaning up of resouces in SceneNode

--- src/engine/SceneNode.ts
+++ src/engine/SceneNode.ts
@@ -24,22 +24,6 @@ export class SceneNode extends BaseNode {
 
   destroy(): void {
     super.destroy();
-
-    if (this.parent) this.parent.removeChild(this);
-    this.scene.traverse((node) => {
-      if (node instanceof THREE.Mesh) {
-        node.geometry.dispose();
-        node.material.dispose();
-      } else if (node instanceof THREE.Light) {
-        node.dispose();
-      } else if (node instanceof THREE.Group) {
-        node.children.forEach((child) => {
-          if (child instanceof THREE.Mesh) {
-            child.geometry.dispose();
-            child.material.dispose();
-          }
-        });
-      }
-    });
+    this.parent?.removeChild(this);
   }
 }
