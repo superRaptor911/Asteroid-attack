4fe060e61bb0edc50486c223c0a3f18edb845510
superRaptor911 <raptor.inc2018@gmail.com>
2022-12-06 21:03:20 +0530

    Moved engine to submodule

--- src/engine/ui/Button.ts
+++
@@ -1,9 +0,0 @@
-import { UI } from '../UI';
-
-export class UIButton extends UI {
-  declare element: HTMLButtonElement;
-  constructor(text: string, className?: string, id?: string) {
-    super('button', className, id);
-    this.element.innerText = text;
-  }
-}
